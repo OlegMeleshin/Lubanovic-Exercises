68db975ddffe6145d0fb7ee9cafd0830ff223afb
OlegMeleshin <70806683+OlegMeleshin@users.noreply.github.com>
2020-09-21 12:02:20 +0300

    Delete Chapter_4_part_5.py

--- Chapter_4_part_5.py
+++
@@ -1,4 +0,0 @@
-'''5. Use dictionary comprehension to create 'squares' dictionary.
-Use range(10) call to get the keys and square them to get values.'''
-squares = {key: key**2 for key in range(10)}
-print(squares)

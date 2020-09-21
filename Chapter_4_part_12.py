fa0a834e9a7ca80ecd11fcdc8b8e47a609eaca0c
OlegMeleshin <70806683+OlegMeleshin@users.noreply.github.com>
2020-09-21 12:01:46 +0300

    Delete Chapter_4_part_12.py

--- Chapter_4_part_12.py
+++
@@ -1,7 +0,0 @@
-'''12. Use Zip function to create 'movies' dictionary which combines following lists:
-titles=['Creature of Habit', 'Crewel Fate'] and plots=['A nun turns into a monster',
-'A haunted yarn shop'].'''
-titles = ['Creature of Habit', 'Crewel Fate']
-plots = ['A nun turns into a monster', 'A haunted yarn shop']
-movies = dict(zip(titles, plots))
-print(movies)

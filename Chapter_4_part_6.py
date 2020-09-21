fe4d7a1ca015f3357afe8b525a7b27b267e635bc
OlegMeleshin <70806683+OlegMeleshin@users.noreply.github.com>
2020-09-21 12:02:27 +0300

    Delete Chapter_4_part_6.py

--- Chapter_4_part_6.py
+++
@@ -1,4 +0,0 @@
-'''6. Use Set comprehension to create the 'odd' set containing even numbers in range(10).
-Так и написано ODD, в котором ЧЕТНЫЕ числа. Опечатка возможно.'''
-odd = {even for even in range(10) if even % 2 == 0}
-print(odd)

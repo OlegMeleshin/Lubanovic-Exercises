12964373ddda9bd2028c85fca1214e921623f092
OlegMeleshin <70806683+OlegMeleshin@users.noreply.github.com>
2020-09-21 12:02:34 +0300

    Delete Chapter_4_part_7.py

--- Chapter_4_part_7.py
+++
@@ -1,7 +0,0 @@
-'''7. Use generator comprehension to return 'Got' string + quantity of numbers
-in range(10). Iterate over it using For loop.'''
-generator1 = (number for number in range(10))
-count = 0
-for number in generator1:
-    count += 1
-print(f"Got {count}")
